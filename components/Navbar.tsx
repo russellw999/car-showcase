13d5c4cff6e45532704772ab0c754296e6aefe64
russellw999 <russellw999@gmail.com>
2023-06-24 19:44:11 +0200

    31.36 added Navbar and Footer to layout.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import CustomButton from './CustomButton'
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  );
+}
+
+export default Navbar;
